d121538c1e49d585bab22b66ed3e310f4646d5d8
willyvaessen <github@willyvaessen.nl>
2023-04-27 14:46:45 +0200

    Firts upload

---
+++ src/main/java/nl/willyvaessen/les10/Les10Application.java
@@ -0,0 +1,13 @@
+package nl.willyvaessen.les10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Les10Application.class, args);
+	}
+
+}
---
+++ src/test/java/nl/willyvaessen/les10/Les10ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.willyvaessen.les10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
